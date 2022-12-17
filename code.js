c61b241177f4b1b6548c1766537410fc916cbfe3
Lin Čadež <konji.op.net@gmail.com>
2022-12-17 13:53:09 +0100

    redisgin

---
+++ code.js
@@ -0,0 +1,8 @@
+function scrollToGallery() {
+    // Find the element with the ID "gallery"
+    const galleryElement = document.getElementById("gallery");
+
+    // Scroll to the element
+
+    galleryElement.scrollIntoView({ behavior: "smooth" });
+  }
